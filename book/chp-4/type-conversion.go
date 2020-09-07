2ee9fa0f6e666a1979083b9bfecaef93a0c011ed
fran-man <fran.man@hotmail.co.uk>
2020-09-07 12:09:18 +0100

    Tidy

--- book/chp-4/type-conversion.go
+++ book/chp-4/type-conversion.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-  var float float64 = 123.123
-  var int int32 = 4
-  fmt.Println(float/float64(int))
+  var myFloat float64 = 123.123
+  var myInt int32 = 4
+  fmt.Println(myFloat/float64(myInt))
 }
