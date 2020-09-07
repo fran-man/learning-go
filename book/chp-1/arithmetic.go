ec180fad02c62081c9ed285bbf23e0984b6d6cdd
fran-man <fran.man@hotmail.co.uk>
2020-09-07 11:10:00 +0100

    Printing a float

--- book/chp-1/arithmetic.go
+++ book/chp-1/arithmetic.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("10 x 9 = ", 10.0 * 9.0)
+  fmt.Println("10 x 9.5 = ", 10.0 * 9.5)
+  fmt.Printf("10000x10000 - 100 = %.2f", 10000.0 * 10000.0 - 100)
 }
