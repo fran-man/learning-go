e5334de8ee8ba6c253ed4c853ec7ba0390d3b85c
fran-man <fran.man@hotmail.co.uk>
2020-09-08 16:37:33 +0100

    Struct Functions

--- book/chp-7/structs.go
+++ book/chp-7/structs.go
@@ -8,9 +8,18 @@ type Rectangle struct {
 
 func main() {
   rect := &Rectangle{10, 11}
-  fmt.Println(area(rect))
+  fmt.Println(rect.area())
+  rect.doubleSize()
+  fmt.Println(rect.area())
+  rect.doubleSize()
+  fmt.Println(rect.area())
 }
 
-func area(r *Rectangle) float64 {
+func (r *Rectangle) area() float64 {
   return r.width*r.height
+}
+
+func (r *Rectangle) doubleSize() {
+  r.width = r.width*2
+  r.height = r.height*2
 }
