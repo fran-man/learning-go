3a818f73b57e8e210cbf2244cde01791e877f5f9
fran-man <fran.man@hotmail.co.uk>
2020-09-09 12:24:07 +0100

    Format using gofmt

--- book/chp-8/TCP/client.go
+++ book/chp-8/TCP/client.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "net"
-  "bufio"
-  "os"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
-  conn,err := net.Dial("tcp","localhost:1234")
-  defer conn.Close()
+	conn, err := net.Dial("tcp", "localhost:1234")
+	defer conn.Close()
 
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-  rdr := bufio.NewReader(os.Stdin)
-  encdr := json.NewEncoder(conn)
+	rdr := bufio.NewReader(os.Stdin)
+	encdr := json.NewEncoder(conn)
 
-  for {
-  fmt.Println("Please enter a message to send: ")
-  msg,e := rdr.ReadString('\n')
+	for {
+		fmt.Println("Please enter a message to send: ")
+		msg, e := rdr.ReadString('\n')
 
-  if e != nil {
-    fmt.Println("Error reading from command line...",e)
-    continue
-  }
+		if e != nil {
+			fmt.Println("Error reading from command line...", e)
+			continue
+		}
 
-  err2 := encdr.Encode(msg)
-  if err2 != nil {
-    fmt.Println("Error sending over network",err2)
-    continue
-  }
-}
+		err2 := encdr.Encode(msg)
+		if err2 != nil {
+			fmt.Println("Error sending over network", err2)
+			continue
+		}
+	}
 }
